d4bb1efa308b16b65f844f4df210d3938344d232
telegrace <telegrace@gmx.de>
2022-03-30 18:03:13 +0200

    feat: moved pokeapi request to server folder

--- server/server.js
+++ server/server.js
@@ -13,15 +13,58 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fastify_1 = __importDefault(require("fastify"));
+const axios_1 = __importDefault(require("axios"));
 const app = (0, fastify_1.default)();
 app.register(require("fastify-cors"), {
     // put your options here
-    origin: "http://localhost:3000",
+    origin: (origin, callback) => {
+        const hostname = new URL(origin).hostname;
+        if (hostname === "localhost") {
+            //  Request from localhost will pass
+            console.log();
+            callback(null, true);
+            return;
+        }
+        // Generate an error on other origins, disabling access
+        callback(new Error("Not allowed"));
+    },
+});
+app.get("/", (req, reply) => {
+    reply.send({ hello: "world" });
 });
 app.get("/ping", (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
     console.log("GET /ping");
-    // reply.send("<!doctype html><title>Hello World!</title><p>Hello World!");
     return "pong\n";
+}));
+app.get("/pokemon", (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
+    axios_1.default
+        .get(`https://pokeapi.co/api/v2/pokemon/pikachu`)
+        .then((response) => {
+        var _a, _b, _c, _d, _e;
+        let abilities = [];
+        if ((_a = response === null || response === void 0 ? void 0 : response.data) === null || _a === void 0 ? void 0 : _a.abilities) {
+            for (let i = 0; i < ((_b = response === null || response === void 0 ? void 0 : response.data) === null || _b === void 0 ? void 0 : _b.abilities.length); i++) {
+                abilities.push(response === null || response === void 0 ? void 0 : response.data.abilities[i].ability.name);
+            }
+        }
+        let types = [];
+        if ((_c = response === null || response === void 0 ? void 0 : response.data) === null || _c === void 0 ? void 0 : _c.types) {
+            for (let i = 0; i < (response === null || response === void 0 ? void 0 : response.data.types.length); i++) {
+                types.push(response === null || response === void 0 ? void 0 : response.data.types[i].type.name);
+            }
+        }
+        let pokemonCardData = {
+            name: "pikachua",
+            abilities,
+            image: (_e = (_d = response === null || response === void 0 ? void 0 : response.data) === null || _d === void 0 ? void 0 : _d.sprites) === null || _e === void 0 ? void 0 : _e.front_default,
+            types,
+        };
+        reply.send(pokemonCardData);
+    })
+        .catch((err) => {
+        console.log(err);
+        reply.statusCode = 500;
+    });
 }));
 app.listen(3001, (err, address) => {
     if (err) {
--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,62 @@
 import fastify from "fastify";
+import axios from "axios";
 
 const app = fastify();
 
+type Callback = (err: Error | null, value?: boolean) => void;
+
 app.register(require("fastify-cors"), {
   // put your options here
-  origin: "http://localhost:3000",
+  origin: (origin: string, callback: Callback) => {
+    const hostname = new URL(origin).hostname;
+    if (hostname === "localhost") {
+      //  Request from localhost will pass
+      console.log();
+      callback(null, true);
+      return;
+    }
+    // Generate an error on other origins, disabling access
+    callback(new Error("Not allowed"));
+  },
+});
+
+app.get("/", (req, reply) => {
+  reply.send({ hello: "world" });
 });
 
 app.get("/ping", async (request, reply) => {
   console.log("GET /ping");
-  // reply.send("<!doctype html><title>Hello World!</title><p>Hello World!");
   return "pong\n";
+});
+
+app.get("/pokemon", async (request, reply) => {
+  axios
+    .get(`https://pokeapi.co/api/v2/pokemon/pikachu`)
+    .then((response) => {
+      let abilities = [];
+      if (response?.data?.abilities) {
+        for (let i = 0; i < response?.data?.abilities.length; i++) {
+          abilities.push(response?.data.abilities[i].ability.name);
+        }
+      }
+      let types = [];
+      if (response?.data?.types) {
+        for (let i = 0; i < response?.data.types.length; i++) {
+          types.push(response?.data.types[i].type.name);
+        }
+      }
+      let pokemonCardData = {
+        name: "pikachua",
+        abilities,
+        image: response?.data?.sprites?.front_default,
+        types,
+      };
+      reply.send(pokemonCardData);
+    })
+    .catch((err) => {
+      console.log(err);
+      reply.statusCode = 500;
+    });
 });
 
 app.listen(3001, (err, address) => {
